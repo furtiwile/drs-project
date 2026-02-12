83462309f313dba7bdae2934d95bb124998b1d49
furtiwile <furtiwile@gmail.com>
2026-02-12 15:14:30 +0100

    vite

--- flight-management-ui/vite.config.ts
+++ flight-management-ui/vite.config.ts
@@ -6,4 +6,4 @@ export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-port: 5173, cors: { origin: ['nozomi.proxy.rlwy.net:17971','http://metro.proxy.rlwy.net:12922', 'http://localhost:5173', 'http://127.0.0.1:5173','http://127.0.0.1:5555','http://127.0.0.1:5555'], methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], credentials: true } } })
+port: 5173, cors: { origin: ['nozomi.proxy.rlwy.net:17971','http://metro.proxy.rlwy.net:12922', 'http://localhost:5173', 'http://127.0.0.1:5173','http://127.0.0.1:5555','http://127.0.0.1:5555','http://127.0.0.1:8000','http://127.0.0.1:8001','http://localhost:8000','http://localhost:8001'], methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'], credentials: true } } })
