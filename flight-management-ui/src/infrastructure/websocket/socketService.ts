935f56361abd3b7cdbefcd402834d79ac83735fc
furtiwile <furtiwile@gmail.com>
2026-02-12 11:55:20 +0100

    trigger front actions

--- flight-management-ui/src/infrastructure/websocket/socketService.ts
+++ flight-management-ui/src/infrastructure/websocket/socketService.ts
@@ -1,6 +1,6 @@
 import { io, Socket } from 'socket.io-client';
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5001';
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:5555';
 
 class SocketService {
   private socket: Socket | null = null;
