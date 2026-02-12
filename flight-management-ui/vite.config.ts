c67a7e0841cdcd824398bc5aba586513045b080a
furtiwile <furtiwile@gmail.com>
2026-02-12 14:19:41 +0100

    update cors

--- flight-management-ui/vite.config.ts
+++ flight-management-ui/vite.config.ts
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+    cors: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+    }
+  }
 })
